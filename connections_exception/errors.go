7489c4a594047d6ddf3a0ec0c76faba31ae7bd9b
Luciano Silva <lurodrisilva@hotmail.com>
2024-11-03 15:45:14 -0300

    adjusting error handling

--- connections_exception/errors.go
+++ connections_exception/errors.go
@@ -5,9 +5,9 @@ import (
 )
 
 var (
-	CedroConnectionError     = exceptioncore.NewTradingError("E001", "Unable to connect to Cedro to obtain Market Data")
-	BufSizeParameterError    = exceptioncore.NewTradingError("E002", "Error converting buffer size parameter, which is an integer")
-	SendingMessageError      = exceptioncore.NewTradingError("E003", "Error sending message over the socket")
-	ReceivingMessageError    = exceptioncore.NewTradingError("E004", "Error receiving message from the socket")
-	SimulationParameterError = exceptioncore.NewTradingError("E005", "Error processing simulation parameter, which is a boolean")
+	CedroConnectionError     = exceptioncore.NewTradingError("CON001", "Unable to connect to Cedro to obtain Market Data")
+	BufSizeParameterError    = exceptioncore.NewTradingError("CON002", "Error converting buffer size parameter, which is an integer")
+	SendingMessageError      = exceptioncore.NewTradingError("CON003", "Error sending message over the socket")
+	ReceivingMessageError    = exceptioncore.NewTradingError("CON004", "Error receiving message from the socket")
+	SimulationParameterError = exceptioncore.NewTradingError("CON005", "Error processing simulation parameter, which is a boolean")
 )
